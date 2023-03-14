7b93fa42c6530d841aaaba9685a0f3e670d358cb
Karuncj <83808984+Karuncj@users.noreply.github.com>
2023-03-14 19:52:31 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -11,3 +11,24 @@ for line in f:
     sum=sum+num
 print("sum=",sum)
 f.close()
+
+
+'''Another Method
+
+f=open("file","w")
+print("Enter 10 numnbers to write to file")
+for i in range(0,10):
+    n=input()
+    f.write(n+" ")
+f.close()
+f=open("file","r")
+sum=0
+for line in f:
+        l=line.split()
+        for j in l:
+            num=int(j)
+            sum=sum+num
+print("sum=",sum)
+f.close()
+
+'''
